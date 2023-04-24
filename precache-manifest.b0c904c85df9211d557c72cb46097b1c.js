9b3f7ffe0e39978f44214f0587a6004a0c097387
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-04-24 03:17:46 +0200

    Updates

---
+++ precache-manifest.b0c904c85df9211d557c72cb46097b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0fc7c52303667dddb26",
+    "url": "/WEB2Y_Assignment2/static/css/main.c2e78350.chunk.css"
+  },
+  {
+    "revision": "f0fc7c52303667dddb26",
+    "url": "/WEB2Y_Assignment2/static/js/main.f0fc7c52.chunk.js"
+  },
+  {
+    "revision": "4e1683194e80a926b0b8",
+    "url": "/WEB2Y_Assignment2/static/js/1.4e168319.chunk.js"
+  },
+  {
+    "revision": "08b8b3b80d6f7f02c3b8",
+    "url": "/WEB2Y_Assignment2/static/js/runtime~main.08b8b3b8.js"
+  },
+  {
+    "revision": "f96a08fa96472031608960914f95beeb",
+    "url": "/WEB2Y_Assignment2/static/media/Logo.f96a08fa.png"
+  },
+  {
+    "revision": "5375ae8ba0bbff9f495af940baa55889",
+    "url": "/WEB2Y_Assignment2/static/media/back.5375ae8b.png"
+  },
+  {
+    "revision": "fcdf2ea9fac8f9fc7d86c03a454fcd6f",
+    "url": "/WEB2Y_Assignment2/index.html"
+  }
+];
